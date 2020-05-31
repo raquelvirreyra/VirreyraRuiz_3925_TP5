a3b84be7aa1a9697573a8a84bf8c99d5b6d19fc6
chevi <chevi@LAPTOP-8O5H664H>
2020-05-31 16:57:57 -0300

    Asunto: Inicializacion del proyecto. Responsable: Ayelén Raquel Virreyra Ruiz. Task00: Inicializacion del proyecto.

---
+++ src/main/java/ar/edu/unju/fi/VirreyraRuiz3925Tp5Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirreyraRuiz3925Tp5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirreyraRuiz3925Tp5Application.class, args);
+	}
+
+}
